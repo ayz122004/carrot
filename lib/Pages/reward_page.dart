4ca478b19f9fe1572aaa6e410d3db9b1aa18c6f5
Anna <leec00260@gmail.com>
2021-07-04 15:57:20 -0700

    appbar colors

--- lib/Pages/reward_page.dart
+++ lib/Pages/reward_page.dart
@@ -26,7 +26,7 @@ class _RewardPageState extends State<RewardPage> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        backgroundColor: Colors.teal[600],
+        backgroundColor: Colors.amber[700],
         title: const Text("Reward Page"),
       ),
       body: ListView(
